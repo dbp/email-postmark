3fcb829bb63648fb3b8943d2f5bf9fc428cb3b4e
Daniel Patterson <dbp@dbpmail.net>
2015-04-15 18:54:37 -0400

    switch from bytestring to text, update deps

--- src/Network/Mail/Postmark.hs
+++ src/Network/Mail/Postmark.hs
@@ -4,19 +4,20 @@ module Network.Mail.Postmark
   (postmark)
   where
 
-import Data.Aeson
-import Data.Aeson.Types
-import qualified Data.Attoparsec as A
+import           Data.Aeson
+import           Data.Aeson.Types
+import qualified Data.Attoparsec      as A
 {-import Control.Monad-}
-import qualified Data.Map as M
-import qualified Data.ByteString.Lazy as LBS 
-import qualified Data.ByteString as BS
+import qualified Data.ByteString      as BS
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Map             as M
+import qualified Data.Text            as T
 {-import qualified Data.ByteString.Char8 as B8-}
-import Data.Maybe (fromJust)
-import Network.HTTP hiding (getRequest)
-import Network.HTTP.Base
-import Network.HTTP.Headers
-import Network.URI (parseURI)
+import           Data.Maybe           (fromJust)
+import           Network.HTTP         hiding (getRequest)
+import           Network.HTTP.Base
+import           Network.HTTP.Headers
+import           Network.URI          (parseURI)
 {-import Control.Monad.Trans-}
 
 
@@ -26,7 +27,7 @@ maybeAesonResult (Success a) = Just a
 strictifyBS = BS.concat . LBS.toChunks
 
 postmark token from subject tag body to = do
-        resp <- simpleHTTP (Request 
+        resp <- simpleHTTP (Request
                               (fromJust $ parseURI "http://api.postmarkapp.com/email")
                               POST
                               [ mkHeader HdrAccept "application/json"
@@ -36,9 +37,9 @@ postmark token from subject tag body to = do
                               rqbdy) >>= getResponseBody
         -- there has to be a cleaner way to do this...
         let val = (parseMaybe (.: "Message")) =<< (maybeAesonResult.fromJSON) =<< (A.maybeResult $ A.parse json $ strictifyBS resp)
-        return $ maybe False (== ("OK"::BS.ByteString)) val
-  where rqbdy = encode $ M.fromList 
-                        [ ("From" :: BS.ByteString, from)
+        return $ maybe False (== ("OK"::T.Text)) val
+  where rqbdy = encode $ M.fromList
+                        [ ("From" :: T.Text, from)
                         , ("To", to)
                         , ("Subject", subject)
                         , ("Tag", tag)
